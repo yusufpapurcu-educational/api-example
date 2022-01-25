745b7b45c32b54e3274573f1eea6f69e399e1fca
Yusuf Papurcu <yusufturhanp@gmail.com>
2022-01-25 22:25:38 +0300

    logger init

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,38 @@
 package logger
+
+import "log"
+
+// Debugf logs a message at level Debug on the standard logger.
+func Debugf(format string, args ...interface{}) {
+
+	log.Printf(format, args...)
+
+}
+
+// Infof logs a message at level Info on the standard logger.
+func Infof(format string, args ...interface{}) {
+
+	log.Printf(format, args...)
+
+}
+
+// Warnf logs a message at level Warn on the standard logger.
+func Warnf(format string, args ...interface{}) {
+
+	log.Printf(format, args...)
+
+}
+
+// Errorf logs a message at level Error on the standard logger.
+func Errorf(format string, args ...interface{}) {
+
+	log.Printf(format, args...)
+
+}
+
+// Fatalf logs a message at level Fatal on the standard logger.
+func Fatalf(format string, args ...interface{}) {
+
+	log.Fatalf(format, args...)
+
+}
