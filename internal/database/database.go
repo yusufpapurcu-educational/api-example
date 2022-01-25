0ce2dae40b3faa3c5c4b1eab78264cf62fc66dd7
Yusuf Papurcu <yusufturhanp@gmail.com>
2022-01-25 22:42:47 +0300

    database init

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,10 @@
 package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func OpenConnection(dsn string) (*gorm.DB, error) {
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
