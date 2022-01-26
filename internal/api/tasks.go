3dd44cf13085cb823cb17b90f287cf8e09b00c97
Yusuf Papurcu <yusufturhanp@gmail.com>
2022-01-26 12:35:01 +0300

    gin added

--- internal/api/tasks.go
+++ internal/api/tasks.go
@@ -1,1 +1,26 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func GetAllTasks(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {}
+}
+
+func CreateTask(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {}
+}
+
+func GetTask(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {}
+}
+
+func UpdateTask(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {}
+}
+
+func DeleteTask(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {}
+}
--- internal/router/router.go
+++ internal/router/router.go
@@ -3,9 +3,22 @@ package router
 import (
 	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/yusufpapurcu/todo-api-example/internal/api"
 	"gorm.io/gorm"
 )
 
 func New(db *gorm.DB) http.Handler {
-	return &http.ServeMux{}
+	handler := gin.Default()
+
+	setRoutes(handler, db)
+	return handler
+}
+
+func setRoutes(handler *gin.Engine, db *gorm.DB) {
+	handler.GET("/task", api.GetAllTasks(db))
+	handler.POST("/task", api.CreateTask(db))
+	handler.GET("/task/:id", api.GetTask(db))
+	handler.PUT("/task/:id", api.UpdateTask(db))
+	handler.DELETE("/task/:id", api.DeleteTask(db))
 }
